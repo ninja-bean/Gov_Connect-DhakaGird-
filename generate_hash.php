9125aa52438277373243d6ad82e9911d798ef1c2
Injabin Alam <163871832+ninja-bean@users.noreply.github.com>
2026-09-17 14:22:40 +0600

    chore: remove orphaned/legacy files (feedback, unban, retrofit, temp) (#15)

--- generate_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-// generate_hash.php
-$passwords = ['admin123','rahim123','police123'];
-foreach($passwords as $pass){
-    echo $pass . " => " . password_hash($pass, PASSWORD_BCRYPT) . "<br>";
-}
-?>
